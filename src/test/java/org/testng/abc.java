32c65e9d0d1a4420831e05389669656a6b781ddc
yaminikoshal <yaminiraveendran1@gmail.com>
2021-04-22 00:02:20 +0530

    updated one file abc

---
+++ src/test/java/org/testng/abc.java
@@ -0,0 +1,11 @@
+package org.testng;
+
+public class abc {
+	public static void main(String[] args) {
+		System.out.println("completed");
+		
+		System.out.println("Updated from master to own");
+	}
+	
+
+}
